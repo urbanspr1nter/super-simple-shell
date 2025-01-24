ee57a987b95f7274332618ab3a106d66fe2d7a11
Roger Ngo <urbanspr1nter@gmail.com>
2025-01-23 18:00:41 -0800

    Example with linenoise

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-sssh:
+all:
 	zig build
+
+clean:
+	rm -rf src/.zig-cache zig-out
--- src/main.c
+++ src/main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include "libcstring/src/c_string.h"
-
+#include "linenoise/linenoise.h"
 
 int main(void) {
-    String* s;
-    s = cstring_create(s, "Hello, world!");
+    String* upArrowSeq;
+    String* downArrowSeq;
 
-    printf("%s\n", s->text);
+    upArrowSeq = cstring_create(upArrowSeq, "\x1b[A");
+    downArrowSeq = cstring_create(downArrowSeq, "\x1b[B");
+
+    String* input;
+    String* token;
+    char* line;
+    
+    while ((line = linenoise("@ ")) != NULL) {
+        input = cstring_create(input, line);
+
+        token = cstring_create(token, "exit");
+        if (cstring_equals(input, token)) {
+            break;
+        }
+        
+        if (input->length > 0) {
+            linenoiseHistoryAdd(input->text);
+            linenoiseHistorySave("history");
+        }
+
+        printf("You typed: %s\n", input->text);
+
+        cstring_free(token);
+        cstring_free(input);
+        linenoiseFree(line); 
+    }
 
     return 0;
 }
